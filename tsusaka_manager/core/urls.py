7a47e0c0bb115ea9713726ac6a5b20d39d2424bf
s-isizak <u308224d@ecs.osaka-u.ac.jp>
2025-10-25 00:05:38 +0900

    feat: add participant edit form

--- tsusaka_manager/core/urls.py
+++ tsusaka_manager/core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("participants/", views.participant_list, name="participant_list"),
     path('add/', views.participant_create, name='participant_create'),
+    path('edit/<int:participant_id>/', views.participant_edit, name='participant_edit'),
 ]
--- tsusaka_manager/core/views.py
+++ tsusaka_manager/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Participant
 from .forms import ParticipantForm
 
@@ -14,4 +14,15 @@ def participant_create(request):
             return redirect('participant_list')
     else:
         form = ParticipantForm()
+    return render(request, 'participants/form.html', {'form': form})
+
+def participant_edit(request, participant_id):
+    participant = get_object_or_404(Participant, pk=participant_id)
+    if request.method == 'POST':
+        form = ParticipantForm(request.POST, instance=participant)
+        if form.is_valid():
+            form.save()
+            return redirect('participant_list')
+    else:
+        form = ParticipantForm(instance=participant)
     return render(request, 'participants/form.html', {'form': form})
