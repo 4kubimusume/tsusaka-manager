4392b74c7d79789b9e17b5baead95dd244d8eddb
s-isizak <u308224d@ecs.osaka-u.ac.jp>
2025-10-24 23:47:01 +0900

    feat: add participant registration form feature

--- tsusaka_manager/core/urls.py
+++ tsusaka_manager/core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("participants/", views.participant_list, name="participant_list"),
+    path('add/', views.participant_create, name='participant_create'),
 ]
--- tsusaka_manager/core/views.py
+++ tsusaka_manager/core/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Participant
+from .forms import ParticipantForm
 
 def participant_list(request):
     participants = Participant.objects.all()
     return render(request, "participants/list.html", {"participants": participants})
+
+def participant_create(request):
+    if request.method == 'POST':
+        form = ParticipantForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('participant_list')
+    else:
+        form = ParticipantForm()
+    return render(request, 'participants/form.html', {'form': form})
