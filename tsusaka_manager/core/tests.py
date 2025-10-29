6fde47bcfacdbeec01f04d92bb1afab814d1cc1b
s-isizak <u308224d@ecs.osaka-u.ac.jp>
2025-10-29 21:47:54 +0900

    test: make minor adjustments to test code

--- tsusaka_manager/core/tests.py
+++ tsusaka_manager/core/tests.py
@@ -24,6 +24,7 @@ class ParticipantViewTest(TestCase):
         response = self.client.get(reverse('participant_list'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "User1")
+        self.assertEqual(Participant.objects.count(), 1)
 
     def test_create_view(self):
         response = self.client.post(reverse('participant_create'), {
@@ -32,4 +33,21 @@ class ParticipantViewTest(TestCase):
             "type": "ob",
         })
         self.assertEqual(response.status_code, 302)  # リダイレクト
+        self.assertRedirects(response, reverse("participant_list"))
         self.assertTrue(Participant.objects.filter(name="New User").exists())
+
+    def test_edit_view(self):
+        response = self.client.post(reverse('participant_edit', args=[self.p1.id]), {
+            "name": "Updated User",
+            "email": "updated@example.com",
+            "type": "student",
+        })
+        self.assertEqual(response.status_code, 302)  # リダイレクト
+        self.p1.refresh_from_db()
+        self.assertEqual(self.p1.name, "Updated User")
+        self.assertEqual(self.p1.email, "updated@example.com")
+
+    # def test_delete_view(self):
+    #     response = self.client.post(reverse('participant_delete', args=[self.p1.id]))
+    #     self.assertEqual(response.status_code, 302)
+    #     self.assertFalse(Participant.objects.filter(id=self.p1.id).exists())
